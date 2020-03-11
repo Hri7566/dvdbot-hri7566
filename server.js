60fdea6d5b6a254ac0cf1ff87197be006df1b2d8
Glitch (hello-express) <none>
2020-03-11 19:42:36 +0000

    🌥⛸ Checkpoint ./server.js:9949330/3452

--- server.js
+++ server.js
@@ -1,34 +1,70 @@
-// server.js
-// where your node app starts
+var http = require("http");
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end(
+      "welcome to hri's bot page :)\nthis is just here so heroku/glitch doesn't freak out"
+    );
+  })
+  .listen(process.env.PORT || 3000);
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+console.log("HTTP Host Established");
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+const MPPClient = require('mpp-client-xt');
+const client = new MPPClient("http://www.multiplayerpiano.com", undefined);
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+client.start();
+
+client.on("hi", () => {
+  client.sendArray([{m:'userset', set:{name:"dvdbot [dvd!help](by Hri7566)"}}]);
+  client.setChannel("✧𝓡𝓟 𝓡𝓸𝓸𝓶✧");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+function chat(string) {
+  client.sendArray([{m:'a', message:string}]);
+}
+
+var ctoggle = true;
+
+client.on("a", (msg) => {
+  let args = msg.a.split(' ');
+  let cmd = args[0].toLowerCase();
+  let argcat = msg.a.substring(cmd.length).trim();
+  
+  switch (cmd) {
+    case "dvd!help":
+      chat("not finished :)");
+      break;
+    case "dvd!cursor"
+  }
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+var pos = {x: -42, y: 5};
+var vel = {x: 2/5, y: 2/7};
+
+var cursor = setInterval(function() {
+    if (ctoggle == true) {
+        client.sendArray([{m:'m', x: client.getOwnParticipant().x = pos.x + 50, y: client.getOwnParticipant().y = pos.y + 50}])
+    } else {
+        pos.x = 200;
+        pos.y = 200;
+    }
 });
+
+var cursorupdate = setInterval(function() {
+    pos.x += vel.x;
+    pos.y += vel.y;
+    if (pos.x >= 50) {
+        vel.x = -vel.x;
+    }
+    if (pos.y >= 50) {
+        vel.y = -vel.y;
+    }
+    if (pos.x <= -50) {
+        vel.x = -vel.x;
+    }
+    if (pos.y <= -50) {
+        vel.y = -vel.y;
+    }
+}, 25);
